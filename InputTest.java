75181c49254327d6436ab4f438fa8d90c712d250
timskii <tim@MacBook-Pro-timskii.local>
2020-04-18 15:31:04 +0600

    finish java

--- InputTest.java
+++ InputTest.java
@@ -1,5 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileWriter;
+
 public class InputTest{
-	public static void main(String[] args){
-		System.out.println("test");
-	}
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Integer:");
+        FileWriter file = new FileWriter("data_java.txt");
+        try {
+            int num = Integer.parseInt(br.readLine());
+            for (int i = 0; i < num ; i++){
+            	file.write("Hello java!\n");
+            }
+        	
+        } catch(NumberFormatException nfe) {
+            System.err.println("Invalid Format!");
+            file.close();
+        }
+        file.close();
+    }
 }
